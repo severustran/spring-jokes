d9081284d40094bcc46e438a97cd1b7b06562ab7
Severus <thanhliem1923@gmail.com>
2021-10-29 01:24:54 +0700

    initialize project

---
+++ src/main/java/vn/liemtt/learning/springjokes/SpringJokesApplication.java
@@ -0,0 +1,13 @@
+package vn.liemtt.learning.springjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJokesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/liemtt/learning/springjokes/SpringJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.liemtt.learning.springjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
